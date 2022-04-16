9d28aa7ca545e293bea40b909c9188a389a2320d
dginovker <danielginovker@gmail.com>
2022-04-17 01:42:20 +0900

    Initialize basic react app with Firebase

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { getDatabase, ref, set } from "firebase/database";
 
 function App() {
+  function writeHelloToDatabase() {
+    const db = getDatabase();
+    set(ref(db, '/'), {
+      "hello": "world"
+    });
+  }
+
+  // Make a button that calls writeHelloToDatabase
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello World!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={writeHelloToDatabase}> Write to database </button>
       </header>
     </div>
   );
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCsibJX8clU1CtNbuBrkQmLJM0XAzDKPOo",
+  authDomain: "bfme-elo.firebaseapp.com",
+  projectId: "bfme-elo",
+  storageBucket: "bfme-elo.appspot.com",
+  messagingSenderId: "587339478118",
+  appId: "1:587339478118:web:3979feb706f0b4ced269a7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
