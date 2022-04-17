bef7e8df877f476768a88fb61c65a971eb21407e
dginovker <danielginovker@gmail.com>
2022-04-18 00:41:16 +0900

    Add database methods for getting and setting multiple ELOs at a time

--- src/database.js
+++ src/database.js
@@ -1,11 +1,56 @@
 import { get, getDatabase, ref, child, set } from "firebase/database";
 
+/**
+ * Add a new score to the database
+ * @param {string} name - Player's name
+ * @param {number} score - Player's score
+ * @returns Promise that resolves when the score is added
+ */
 export const updateScoreInDb = (name, score) => {
   const db = getDatabase();
   return set(ref(db, `/elo/${name}`), score);
 };
 
+/**
+ * Return a score from the database
+ * @param {string} name - Player's name
+ * @returns Promise that resolves to a snapshot of the name's score
+ * @example getScoreFromDb("test1").then((snapshot) => {
+ *  expect(snapshot.val()).toBe(50);
+ * });
+ */
 export const getScoreFromDb = (name) => {
   const db = getDatabase();
   return get(child(ref(db), `/elo/${name}`));
+};
+
+/**
+ * Adds multiple scores to the database
+ * @param {Object[]} scores - Array of objects with name and score properties
+ * @returns Promise that resolves when all scores are added
+ * @example updateScoresInDb([{ name: "test1", score: 50 }, { name: "test2", score: 100 }]);
+ */
+export const updateScoresInDb = (scores) => {
+  // Add all the scores, return a promise
+  return scores.reduce((promise, score) => {
+    return promise.then(() => {
+      return updateScoreInDb(score.name, score.score);
+    });
+  }, Promise.resolve());
+};
+
+/**
+ * Returns the score for multiple players.
+ * @param {string[]} names - array of names to get the scores for
+ * @returns Promise that resolves to an array of snapshots of the scores
+ * @example getScoresFromDb(["test1", "test2"]).then(snapshots => {
+ *   expect(snapshots[0].val()).toBe(50);
+ *   expect(snapshots[1].val()).toBe(100);
+ * });
+ */
+export const getScoresFromDb = (names) => {
+  // Get all the scores async
+  return Promise.all(names.map((name) => {
+    return getScoreFromDb(name);
+  }));
 };
--- src/database.test.js
+++ src/database.test.js
@@ -1,5 +1,6 @@
-import { updateScoreInDb, getScoreFromDb } from "./database.js";
+import { getScoreFromDb, getScoresFromDb, updateScoreInDb, updateScoresInDb } from "./database.js";
 import { initializeFirebase, terminateFirebase } from "./firebase.js";
+import { maxElo, minElo } from "./elo";
 
 // Before running any tests, initialize Firebase
 initializeFirebase();
@@ -31,9 +32,9 @@ test('Write invalid value (object) to database then expect the value to remain u
   });
 });
 
-test('Write invalid value (negative number) to database then expect the value to remain unchanged', done => {
+test('Write invalid value (number below minElo) to database then expect the value to remain unchanged', done => {
   jest.spyOn(console, 'warn').mockImplementation(() => {});
-  updateScoreInDb("test4", -1).catch(() => {
+  updateScoreInDb("test4", minElo - 1).catch(() => {
     expect(console.warn).toHaveBeenCalledTimes(1);
     console.warn.mockRestore();
     done();
@@ -41,12 +42,22 @@ test('Write invalid value (negative number) to database then expect the value to
 });
 
 
-test('Write invalid value (number over 2500) to database then expect the value to remain unchanged', done => {
+test('Write invalid value (number over maxElo) to database then expect the value to remain unchanged', done => {
   jest.spyOn(console, 'warn').mockImplementation(() => {});
-  updateScoreInDb("test5", 2501).catch(() => {
+  updateScoreInDb("test5", maxElo + 1).catch(() => {
     expect(console.warn).toHaveBeenCalledTimes(1);
     console.warn.mockRestore();
     done();
+  });
+});
+
+test('Write multiple values to database with updateScoresInDb then expect to see those values with getScoresFromDb', done => {
+  updateScoresInDb([{ name: "test6", score: 50 }, { name: "test7", score: 100 }]).then(() => {
+    getScoresFromDb(["test6", "test7"]).then((snapshots) => {
+      expect(snapshots[0].val()).toBe(50);
+      expect(snapshots[1].val()).toBe(100);
+      done();
+    });
   });
 });
 
