b31e27c59288019b2da98fb20e9bec736c92b8d4
dginovker <danielginovker@gmail.com>
2022-04-18 00:41:51 +0900

    Add ELO constants and a new method to get updated ELOs easily

--- src/elo.js
+++ src/elo.js
@@ -1,5 +1,31 @@
+export const maxElo = 2500;
+export const minElo = 0;
+
+/**
+ * Calculates the change in ELO for a given score
+ * @param {number} winnerElo 
+ * @param {number} loserElo 
+ * @returns The amount each ELO should change by
+ */
 export const getEloChange = (winnerElo, loserElo) => {
   const k = 32;
   const e = 1 / (1 + Math.pow(10, (loserElo - winnerElo) / 400));
   return k * (1 - e);
+};
+
+/**
+ * Calculates the new ELO for users after a game
+ * @param {number} winnerElo 
+ * @param {number} loserElo 
+ * @returns Array of new ELOs
+ */
+export const getNewElo = (winnerElo, loserElo) => {
+  if (winnerElo == null) {
+    winnerElo = 1200;
+  }
+  if (loserElo == null) {
+    loserElo = 1200;
+  }
+  const eloChange = getEloChange(winnerElo, loserElo);
+  return [Math.min(winnerElo + eloChange, maxElo), Math.max(loserElo - eloChange, minElo)];
 };
--- src/elo.test.js
+++ src/elo.test.js
@@ -1,4 +1,4 @@
-import { getEloChange } from "./elo";
+import { getEloChange, getNewElo, maxElo } from "./elo";
 
 test('ELO cannot change by more than 32', () => {
   expect(getEloChange(50, 2500)).toBeLessThanOrEqual(32);
@@ -6,4 +6,11 @@ test('ELO cannot change by more than 32', () => {
 
 test('ELO change for same score is 16', () => {
   expect(getEloChange(50, 50)).toBe(16);
+});
+
+test('Updated ELO is correct', () => {
+  expect(getNewElo(50, 50)).toEqual([66, 34]);
+  expect(getNewElo(1200, 1200)).toEqual([1216, 1184]);
+  const [winnerElo, loserElo] = getNewElo(2500, 2500);
+  expect(winnerElo).toBe(maxElo);
 });
