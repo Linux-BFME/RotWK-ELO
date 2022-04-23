2e922717edb5f3b149bac63b4b5f6a4a0fd87da8
dginovker <danielginovker@gmail.com>
2022-04-24 01:24:44 +0900

    Change how background image is added, blur it

--- src/App.js
+++ src/App.js
@@ -17,7 +17,31 @@ function App() {
 
   return (
     <div>
-      <div className="App" style={{cursor: 'url(images/cursor.png),auto'}}>
+      <div style={{
+        backgroundImage: 'url(images/background.webp)',
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        // Add blur effect to background
+        filter: 'blur(5px)',
+        // Take up whole page
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        zIndex: -1,
+        // Don't move on scroll
+        transform: 'translateZ(0)',
+
+
+      }}/>
+      <div className="App"
+        id="App"
+        style={{
+          cursor: 'url(images/cursor.png),auto',
+        }}
+      >
         <header className="App-header">
         BFME ELO
         </header>
